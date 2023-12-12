24c7ef8bc1326ec0479336e6a3ee6ae9d98a98b8
ImperialSquid <benjaeger99@gmail.com>
2023-12-12 17:58:30 +0000

    Solve 2023 day 4

--- 2023/day_04/solution.py
+++ 2023/day_04/solution.py
@@ -1,2 +1,28 @@
+
+data = list()
 with open("data.txt") as f:
-    pass
+    for line in f:
+        clean = line.split(": ")[1].strip().replace("  ", " ")
+        data.append(clean)
+
+point_total = 0
+copies_dict = {c: 1 for c in range(len(data))}
+
+for index, item in enumerate(data):
+    cards = item.split(" | ")
+
+    win_set = {int(num) for num in cards[0].split(" ")}
+    you_set = {int(num) for num in cards[1].split(" ")}
+
+    matches = len(win_set.intersection(you_set))
+
+    if matches:
+        point_total += 2 ** (matches - 1)
+
+        for i in range(matches, 0, -1):
+            copies_dict[index + i] = copies_dict.get(index + i, 0) + copies_dict.get(index, 1)
+
+copies_tptal = sum(copies_dict.values())
+
+print("Solution 1: " + str(point_total))
+print("Solution 2: " + str(copies_tptal))
