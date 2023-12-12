e262722d56fac707af9800a80e571e8dfc4903e4
ImperialSquid <benjaeger99@gmail.com>
2023-12-12 00:25:08 +0000

    Create code for making files

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+year = input("AoC Yea: ")
+
+dirs = [year,] + [os.path.join(year, "day_"+str(day+1).zfill(2)) for day in range(25)]
+for dir in dirs:
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+for day in dirs[1:]:
+    with open(os.path.join(day, "data.txt"), "w+") as f:
+        pass
+
+    with open(os.path.join(day, "solution.py"), "w+") as f:
+        f.writelines(["with open(\"data.txt\") as f:\n",
+                      "    pass\n"])
