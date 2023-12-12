8509b997e696046f723d35c2e7ee2cc276c2925c
ImperialSquid <benjaeger99@gmail.com>
2023-12-12 17:17:43 +0000

    Solve 2023 day 3

--- 2023/day_03/solution.py
+++ 2023/day_03/solution.py
@@ -1,2 +1,39 @@
+import re
+
+lines = ["." * 1000]
 with open("data.txt") as f:
-    pass
+    for line in f:
+        lines.append("." + line.strip() + ".")
+lines.append("." * 1000)  # padding for later scanning to avoid try/except block
+
+id_total = 0
+gear_dict = dict()
+
+for index, line in enumerate(lines):
+    for match in re.finditer("[0-9]+", line):
+        num = int(match.group())
+        span = match.span()
+
+        text = lines[index - 1][span[0]-1: span[1]+1] + \
+               lines[index][span[0]-1: span[1]+1] + \
+               lines[index + 1][span[0]-1: span[1]+1]
+
+        symbol = re.search("[^.0-9]", text)
+        if symbol:
+            id_total += num
+
+            if symbol.group() == "*":
+                # use relative offet to find gear absolute pos
+                pos_off = symbol.start() % (span[1] - span[0] + 2) - 1
+                inx_off = symbol.start() // (span[1] - span[0] + 2) - 1
+                gear_pos = str(index + inx_off) + "-" + str(match.start() + pos_off)
+
+                gear_dict[gear_pos] = gear_dict.get(gear_pos, list()) + [num, ]
+
+print(gear_dict)
+
+gear_ratio_sum = sum([v[0] * v[1] if len(v) == 2 else 0
+                      for v in gear_dict.values()])
+
+print("Solution 1: " + str(id_total))
+print("Solution 2: " + str(gear_ratio_sum))
