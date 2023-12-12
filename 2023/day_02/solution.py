fafdd640c5adba8b2944d7e9a0737abf4bb14a3a
ImperialSquid <benjaeger99@gmail.com>
2023-12-12 15:32:55 +0000

    Solve 2023 day 2

--- 2023/day_02/solution.py
+++ 2023/day_02/solution.py
@@ -1,2 +1,20 @@
 with open("data.txt") as f:
-    pass
+    poss_id_total = 0
+    power_total = 0
+
+    for line in f:
+        counts = {"red": 0, "green": 0, "blue": 0}
+        id_, data = line.strip().split(": ")
+
+        for game in data.split("; "):
+            for colour in game.split(", "):
+                counts[colour.split(" ")[1]] = max(counts[colour.split(" ")[1]],
+                                                   int(colour.split(" ")[0]))
+
+        if counts["red"] <= 12 and counts["green"] <= 13 and counts["blue"] <= 14:
+            poss_id_total += int(id_.split(" ")[1])
+
+        power_total += counts["red"] * counts["green"] * counts["blue"]
+
+    print("Solution 1: " + str(poss_id_total))
+    print("Solution 2: " + str(power_total))
