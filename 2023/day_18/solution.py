90da7a81d8f83434a99a1f45f930b1a178aa281c
Ben Jaeger <benjaeger99@gmail.com>
2024-01-01 04:27:27 +0000

    Solve 2023 day 18

--- 2023/day_18/solution.py
+++ 2023/day_18/solution.py
@@ -1,2 +1,39 @@
+from itertools import pairwise, accumulate
+
 with open("data.txt") as f:
-    pass
+    data = f.read().splitlines()
+
+
+# TODO part 1 and 2 parsing could be combined, but it's solved now
+def parse_part1(line):
+    direction, distance, _ = line.split()
+    delta = int(distance)
+    return ((0, -delta), (delta, 0), (0, delta), (-delta, 0))['URDL'.index(direction)]
+
+
+def parse_part2(line):
+    _, _, colour = line.split()
+    delta = int(colour[2:7], 16)
+    return ((delta, 0), (0, delta), (-delta, 0), (0, -delta))[int(colour[7])]
+
+
+def dig(inst):
+    xs, ys = zip(*inst)  # separate out x and y
+    xs = accumulate(xs)  # get each pos relative to the last
+    ys = accumulate(ys)
+    xy = list(zip(xs, ys))  # repackage back into pairs
+    # print(xy)
+
+    # shoelace formula to get double the internal areas
+    double_area = sum(x1 * y2 - y1 * x2 for (x1, y1), (x2, y2) in pairwise(xy + xy[:1]))
+    boundary = sum(abs(x2 - x1 + y2 - y1) for (x1, y1), (x2, y2) in pairwise(xy + xy[:1]))
+
+    # use Pick's to derive actual area with boundary
+    return (double_area + boundary) // 2 + 1
+
+
+part1_inst = map(parse_part1, data)
+part2_inst = map(parse_part2, data)
+
+print("Solution 1:", dig(part1_inst))
+print("Solution 2:", dig(part2_inst))
