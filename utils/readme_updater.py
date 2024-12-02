bf5446d62ca90e625556666c35e796426ce16b5c
Ben Jaeger <benjaeger99@gmail.com>
2024-12-02 15:50:17 +0000

    feat: add readme_updater.py to update README.md

---
+++ utils/readme_updater.py
@@ -0,0 +1,15 @@
+year = input("AoC Year: ")
+
+with open("../README.md", "a") as readme:
+    readme.write(f"\n\n## { year }\n\n")
+
+    readme.write("<details><summary>Python 00/50</summary>\n\n")
+
+    readme.write("| Day                             | Part 1             | Part 2             |\n")
+    readme.write("|---------------------------------|--------------------|--------------------|\n")
+
+    for day in range(25):
+        num = str(day+1).zfill(2)
+        readme.write(f"| [{ num }](./{ year }/day_{ num }/solution.py) | :x:                | :x:                |\n")
+
+    readme.write("\n</details>")
