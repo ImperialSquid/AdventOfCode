7e6fdee59403c62183069a1071cf30f6485c3609
Ben Jaeger <benjaeger99@gmail.com>
2024-12-02 15:23:50 +0000

    revert: revert putting language solutions in their own dirs

--- utils/template_maker.py
+++ utils/template_maker.py
@@ -3,14 +3,13 @@ import os
 year = input("AoC Year: ")
 
 dirs = ([os.path.join("..", year), ] +
-        [os.path.join("..", year, "python"), ] +
-        [os.path.join("..", year, "python", "day_"+str(day+1).zfill(2)) for day in range(25)])
+        [os.path.join("..", year, "day_"+str(day+1).zfill(2)) for day in range(25)])
 
 for dir in dirs:
     if not os.path.exists(dir):
         os.mkdir(dir)
 
-for day in dirs[2:]:
+for day in dirs[1:]:
     with open(os.path.join(day, "data.txt"), "w+") as f:
         pass
 
