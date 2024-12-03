2eb2ecb46da356050c66369c8c02810e40313047
Ben Jaeger <benjaeger99@gmail.com>
2024-12-03 12:58:17 +0000

    solve: 2024 day 3

--- 2024/day_03/solution.py
+++ 2024/day_03/solution.py
@@ -1,2 +1,27 @@
+import re
+
 with open("data.txt") as f:
-    pass
+    lines = f.readlines()
+
+# === part 1 & 2
+
+p1_total = 0
+p2_total = 0
+mul_enabled = True
+
+for line in lines:
+    # match on either a mul(XX,YY) or do()/don't() statement
+    # groups:
+    #   0 - XX
+    #   1 - YY
+    #   2 - do()/don't()
+    for match in re.finditer(r"mul\((\d+),(\d+)\)|(do(?:n\'t)?\(\))", line):
+        if match.groups()[2]:
+            mul_enabled = "do()" == match.groups()[2]
+        else:
+            p1_total += int(match.groups()[0]) * int(match.groups()[1])
+            if mul_enabled:
+                p2_total += int(match.groups()[0]) * int(match.groups()[1])
+
+print(f"Part 1 Solution: { p1_total }")
+print(f"Part 2 Solution: { p2_total }")
