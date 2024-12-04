ce177f6111cbfd8237b26623ad431963ca3051a6
Ben Jaeger <benjaeger99@gmail.com>
2024-12-04 13:56:12 +0000

    feat: compile all setup files into one

--- utils/readme_updater.py
+++ utils/readme_updater.py
@@ -1,15 +1,19 @@
-year = input("AoC Year: ")
+def update_readme(year: str):
+    with open("../README.md", "a") as readme:
+        readme.write(f"\n\n## { year }\n\n")
 
-with open("../README.md", "a") as readme:
-    readme.write(f"\n\n## { year }\n\n")
+        readme.write("<details><summary>Python 00/50</summary>\n\n")
 
-    readme.write("<details><summary>Python 00/50</summary>\n\n")
+        readme.write("| Day                             | Part 1             | Part 2             |\n")
+        readme.write("|---------------------------------|--------------------|--------------------|\n")
 
-    readme.write("| Day                             | Part 1             | Part 2             |\n")
-    readme.write("|---------------------------------|--------------------|--------------------|\n")
+        for day in range(25):
+            num = str(day+1).zfill(2)
+            readme.write(f"| [{ num }](./{ year }/day_{ num }/solution.py) | :x:                | :x:                |\n")
 
-    for day in range(25):
-        num = str(day+1).zfill(2)
-        readme.write(f"| [{ num }](./{ year }/day_{ num }/solution.py) | :x:                | :x:                |\n")
+        readme.write("\n</details>")
 
-    readme.write("\n</details>")
+if __name__ == "__main__":
+    year = input("AoC Year: ")
+
+    update_readme(year)
