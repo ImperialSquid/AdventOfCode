626eefd2a8d1401bbeda4c5d91d210ee919cf675
Ben Jaeger <benjaeger99@gmail.com>
2024-12-04 13:41:26 +0000

    feat: template_maker.py now requires override before overwriting files that exist

--- utils/template_maker.py
+++ utils/template_maker.py
@@ -1,6 +1,7 @@
 import os
 
 year = input("AoC Year: ")
+force_remake = input("Force remake? (y/n): ")
 
 dirs = ([os.path.join("..", year), ] +
         [os.path.join("..", year, "day_"+str(day+1).zfill(2)) for day in range(25)])
@@ -10,9 +11,18 @@ for dir in dirs:
         os.mkdir(dir)
 
 for day in dirs[1:]:
-    with open(os.path.join(day, "data.txt"), "w+") as f:
-        pass
+    data = os.path.join(day, "data.txt")
+    solution = os.path.join(day, "solution.py")
 
-    with open(os.path.join(day, "solution.py"), "w+") as f:
-        f.writelines(["with open(\"data.txt\") as f:\n",
-                      "    pass\n"])
+    if not os.path.exists(data) or force_remake == "y":
+        with open(data, "w+") as f:
+            pass
+    else:
+        print(f"{ data } already exists")
+
+    if not os.path.exists(solution) or force_remake == "y":
+        with open(solution, "w+") as f:
+            f.writelines(["with open(\"data.txt\") as f:\n",
+                          "    pass\n"])
+    else:
+        print(f"{ solution } already exists")
