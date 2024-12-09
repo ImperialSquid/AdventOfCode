e3f089970084ac9ddd9f8d1f62eda1e59ea9ff65
Ben Jaeger <benjaeger99@gmail.com>
2024-12-09 19:22:41 +0000

    chore: 2024 day 9 - better comments and formatting, add timing

--- 2024/day_09/solution.py
+++ 2024/day_09/solution.py
@@ -17,6 +17,7 @@ with open("data.txt") as f:
             # array of [value, size, position] triplets
             datas.append([i//2+1, int(d), cum_pos])
         else:
+            # need to update size and position of frees so these can't be tuples
             frees.append([0, int(d), cum_pos])
 
         cum_pos += int(d)
@@ -48,13 +49,17 @@ def solve_p2(cells, datas, frees):
         # print("".join([str(c-1) if c else "." for c in cells]))
 
         for free_i in range(len(frees)):
+            # data doesn't have a free before it, break loop
             if frees[free_i][2] > data[2]:
                 break
 
             if frees[free_i][1] >= data[1]:
+                # set values in cell list
                 for i in range(data[1]):
                     cells[frees[free_i][2] + i] = data[0]
                     cells[data[2] + i] = 0
+
+                # update free size and position
                 frees[free_i][1] -= data[1]
                 frees[free_i][2] += data[1]
 
